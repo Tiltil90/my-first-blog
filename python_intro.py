0116072f3a9879e00318ce954c681872996ad4e6
Yuliya Zheltukhina <kukurekup@mail.ru>
2017-03-04 15:19:25 +0300

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,13 @@
+def hi (name):
+	print('Hi ' + name + '!')
+	
+# girls = ['yluiya','lena','katya','olya']
+# for name in girls:
+# 	hi (name)
+# 	print ('next girl')
+
+# name = input('Кто ты? ')
+# hi(name)
+for i in range (1,6):
+	print (i)
+	
